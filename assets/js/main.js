e581211db15dd3c91ee8d3355f1a5c71eb183ca1
DianaQuispe <danaalienmas@gmail.com>
2017-08-09 14:10:59 -0500

    act

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+var botonLi = document.getElementById('botonLi');
+botonLi.onclick = function() {
+	alert("cosita");
+}
+var input = document.getElementById("input").value;
+var array = [];
+var lista = {
+	
+}
